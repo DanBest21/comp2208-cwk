73af5f2f5d150cacb5586c2696b6001e3ea8a790
db5n17 <db5n17@soton.ac.uk>
2019-11-20 16:06:23 +0000

    Removed some test code.

--- src/Search/AStar.java
+++ src/Search/AStar.java
@@ -1,17 +1,13 @@
 package Search;
 
-import Tree.Direction;
 import Tree.Node;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class AStar extends Search
 {
     private final Queue<Node> fringe = new PriorityQueue<>();
-    private final List<Node> optimalSolution = new ArrayList<>();
 
     public Node search()
     {
@@ -42,43 +38,9 @@ public class AStar extends Search
                 childNode.setHeuristic(heuristic);
 
                 fringe.add(childNode);
-                checkOptimalSolution(childNode);
             }
         }
 
         return null;
-    }
-
-    // TEMPORARY
-    private void checkOptimalSolution(Node node)
-    {
-        if (optimalSolution.size() == 0 && node.getValue().getLastDirection() == Direction.UP)
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 1 && node.getValue().getLastDirection() == Direction.LEFT && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 2 && node.getValue().getLastDirection() == Direction.LEFT && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 3 && node.getValue().getLastDirection() == Direction.DOWN && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 4 && node.getValue().getLastDirection() == Direction.LEFT && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 5 && node.getValue().getLastDirection() == Direction.UP && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 6 && node.getValue().getLastDirection() == Direction.RIGHT && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 7 && node.getValue().getLastDirection() == Direction.DOWN && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 8 && node.getValue().getLastDirection() == Direction.RIGHT && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 9 && node.getValue().getLastDirection() == Direction.UP && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 10 && node.getValue().getLastDirection() == Direction.UP && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 11 && node.getValue().getLastDirection() == Direction.LEFT && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 12 && node.getValue().getLastDirection() == Direction.DOWN && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
-        else if (optimalSolution.size() == 13 && node.getValue().getLastDirection() == Direction.LEFT && node.getParent() == optimalSolution.get(optimalSolution.size() - 1))
-            optimalSolution.add(node);
     }
 }
